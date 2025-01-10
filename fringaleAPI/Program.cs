ae9899eb603d68a5a2b9d7835ca234633091c270
DubertJulie <dbt.julie@gmail.com>
2025-01-10 16:53:47 +0100

    Modification des routes vers TypedResults

--- fringaleAPI/Program.cs
+++ fringaleAPI/Program.cs
@@ -43,8 +43,6 @@ if (app.Environment.IsDevelopment())
     });
 }
 
-
-
 // Initialiser la base de donn�es mon json (méthode seed)
 using (var scope = app.Services.CreateScope())
 {
@@ -55,32 +53,42 @@ using (var scope = app.Services.CreateScope())
 }
 
 
-
-
 // ROUTES
 
+// MAP GROUP CLIENT
+var gestionClients = app.MapGroup("/clients").WithTags("Gestion clients");
+gestionClients.MapGet("/", GetAllClients);
+gestionClients.MapGet("/{id}", GetClientById);
+gestionClients.MapPost("/", CreateClient);
+gestionClients.MapPut("/{id}", UpdateClient);
+gestionClients.MapDelete("/{id}", DeleteClient);
+
 // Récupérer tous les clients
-app.MapGet("/clients", async (FringaleAPIDb db) =>
-    await db.Clients.ToListAsync());
+static async Task<IResult> GetAllClients(FringaleAPIDb db)
+{
+    return TypedResults.Ok(await db.Clients.ToListAsync());
+}
 
 // Récupérer un client par son ID
-app.MapGet("/Clients/{id}", async (int id, FringaleAPIDb db) =>
-    await db.Clients.FindAsync(id)
-        is Client client
-            ? Results.Ok(client)
-            : Results.NotFound());
+static async Task<IResult> GetClientById(int id, FringaleAPIDb db)
+{
+    return await db.Clients.FindAsync(id)
+       is Client client
+           ? TypedResults.Ok(client)
+           : TypedResults.NotFound();
+}
 
 // Créer un client 
-app.MapPost("/clients", async (Client client, FringaleAPIDb db) =>
-    {
-        db.Clients.Add(client);
-        await db.SaveChangesAsync();
+static async Task<IResult> CreateClient(Client client, FringaleAPIDb db)
+{
+    db.Clients.Add(client);
+    await db.SaveChangesAsync();
 
-        return Results.Created($"/clients/{client.Id_cl}", client);
-    });
+    return TypedResults.Created($"/clients/{client.Id_cl}", client);
+}
 
-// Modifier un client par son ID
-app.MapPut("/clients/{id}", async (int id, Client inputClient, FringaleAPIDb db) =>
+//// Modifier un client par son ID
+static async Task<IResult> UpdateClient(int id, Client inputClient, FringaleAPIDb db)
 {
     var client = await db.Clients.FindAsync(id);
 
@@ -93,126 +101,170 @@ app.MapPut("/clients/{id}", async (int id, Client inputClient, FringaleAPIDb db)
 
     await db.SaveChangesAsync();
 
-    return Results.NoContent();
+    return TypedResults.NoContent();
+}
+
+// Supprimer un client par son ID 
+static async Task<IResult> DeleteClient(int id, FringaleAPIDb db)
+{
+    if (await db.Clients.FindAsync(id) is Client client)
+    {
+        db.Clients.Remove(client);
+        await db.SaveChangesAsync();
+        return TypedResults.NoContent();
+    }
+
+    return TypedResults.NotFound();
+}
+
+//Récupérer toutes les commandes d'un client 
+gestionClients.MapGet("/{id}/commandes", async (int id, FringaleAPIDb db) =>
+{
+    var client = await db.Clients.FindAsync(id);
+    if (client == null) return Results.NotFound();
+
+    var commandes = await db.Commandes
+                            .Where(o => o.Id_cl == id)
+                            .ToListAsync(); // requ�te LINQ 
+
+    return Results.Ok(commandes);
 });
+
+var gestionCommandes = app.MapGroup("/commandes").WithTags("Gestion commandes");
+gestionCommandes.MapGet("/", GetAllCommands);
+gestionCommandes.MapGet("/{id}", GetCommandbyId);
+gestionCommandes.MapGet("/{date_co}", GetCommandbyDate);
+gestionCommandes.MapPost("/", CreateCommand);
+gestionCommandes.MapPut("/{id}", UpdateCommand);
+gestionCommandes.MapDelete("/{id}", DeleteCommand);
 
 
 // Récupérer toutes les commandes
-app.MapGet("/commandes", async (FringaleAPIDb db) =>
-    await db.Commandes.ToListAsync());
+static async Task<IResult> GetAllCommands(FringaleAPIDb db)
+{
+    return TypedResults.Ok(await db.Commandes.ToListAsync());
+}
 
 // Récupérer une commande par son ID
-app.MapGet("/commandes/{id}", async (int id, FringaleAPIDb db) =>
-    await db.Commandes.FindAsync(id)
+static async Task<IResult> GetCommandbyId(int id, FringaleAPIDb db)
+{
+    return await db.Commandes.FindAsync(id)
         is Commande commande
-            ? Results.Ok(commande)
-            : Results.NotFound());
+            ? TypedResults.Ok(commande)
+            : TypedResults.NotFound();
+}
 
-//Récupérer une commande par sa DATE
-app.MapGet("/commandes/by/{date_co}", async (DateTime date_co, FringaleAPIDb db) =>
-    { var commandes = await db.Commandes.Where(c => c.Date_co.Date == date_co).ToListAsync();
+// Récupérer une commande par sa date
+static async Task<IResult> GetCommandbyDate(DateTime date_co, FringaleAPIDb db)
+{
+    var commandes = await db.Commandes.Where(c => c.Date_co.Date == date_co).ToListAsync();
+    return TypedResults.Ok(commandes);
+}
 
-        return Results.Ok(commandes);
-    }
-);
+// Créer une commande
+static async Task<IResult> CreateCommand(Commande commande, FringaleAPIDb db)
+{
+    db.Commandes.Add(commande);
+    await db.SaveChangesAsync();
 
+    return TypedResults.Created($"/clients/{commande.Id_co}", commande);
+}
 
-// Créer une commande 
-app.MapPost("/commandes", async (Commande commande, FringaleAPIDb db) =>
-    {
-        db.Commandes.Add(commande);
-        await db.SaveChangesAsync();
-
-        return Results.Created($"/clients/{commande.Id_co}", commande);
-    });
 
 // Modifier une commande
-app.MapPut("/commandes/{id}", async (int id, Commande inputCommande, FringaleAPIDb db) =>
+
+static async Task<IResult> UpdateCommand(int id, Commande inputCommande, FringaleAPIDb db)
 {
     var commande = await db.Commandes.FindAsync(id);
 
-    if (commande is null) return Results.NotFound();
+    if (commande is null) return TypedResults.NotFound();
 
     commande.Montant_co = inputCommande.Montant_co;
     commande.Date_co = inputCommande.Date_co;
 
     await db.SaveChangesAsync();
-    return Results.NoContent();
+    return TypedResults.NoContent();
 }
-);
+
+
 
 // Supprimer une commande par son ID
-app.MapDelete("/commandes/{id}", async (int id, FringaleAPIDb db) =>
+static async Task<IResult> DeleteCommand(int id, FringaleAPIDb db)
 {
     if (await db.Commandes.FindAsync(id) is Commande commande)
     {
         db.Commandes.Remove(commande);
         await db.SaveChangesAsync();
-        return Results.NoContent();
+        return TypedResults.NoContent();
     }
-    return Results.NotFound();
-});
 
-//Récupérer toutes les commandes d'un client 
-app.MapGet("/Clients/{id}/commandes", async (int id, FringaleAPIDb db) =>
-{
-    var client = await db.Clients.FindAsync(id);
-if (client == null) return Results.NotFound();
+    return TypedResults.NotFound();
+}
 
-var commandes = await db.Commandes
-                        .Where(o => o.Id_cl == id)
-                        .ToListAsync(); // requ�te LINQ 
 
-return Results.Ok(commandes);
-});
+// MAP GROUP PLATS
+var gestionPlats = app.MapGroup("/plats").WithTags("Gestion plats");
+gestionPlats.MapGet("/", GetAllPlats);
+gestionPlats.MapGet("/{id}", GetPlatbyId);
+gestionPlats.MapPost("/", CreatePlat);
+gestionPlats.MapPut("/{id}", UpdatePlat);
+gestionPlats.MapDelete("/{id}", DeletePlat);
 
 // Récupérer les plats 
-app.MapGet("/Plats", async (FringaleAPIDb db) =>
-    await db.Plats.ToListAsync());
+static async Task<IResult> GetAllPlats(FringaleAPIDb db)
+{
+    return TypedResults.Ok(await db.Plats.ToArrayAsync());
+}
+
 
 // Récupérer les plats par ID 
-app.MapGet("/Plats/{id_pl}", async (int id_pl, FringaleAPIDb db) =>
-    await db.Plats.FindAsync(id_pl)
-        is Plat plat
-            ? Results.Ok(plat)
-            : Results.NotFound());
+static async Task<IResult> GetPlatbyId(int id, FringaleAPIDb db)
+{
+    return await db.Plats.FindAsync(id)
+       is Plat plat
+           ? TypedResults.Ok(plat)
+           : TypedResults.NotFound();
+}
 
 // Créer un plat 
-app.MapPost("/Plats", async (Plat plat, FringaleAPIDb db) =>
+static async Task<IResult> CreatePlat(Plat plat, FringaleAPIDb db)
 {
     db.Plats.Add(plat);
     await db.SaveChangesAsync();
 
-    return Results.Created($"/Plats/{plat.Id_pl}", plat);
-});
+    return TypedResults.Created($"/clients/{plat.Id_pl}", plat);
+}
 
-// Modifier un plat 
-app.MapPut("/Plats/{id_pl}", async (int id_pl, Plat inputPlat, FringaleAPIDb db) =>
+
+// Modifier un plat
+
+static async Task<IResult> UpdatePlat(int id, Plat inputPlat, FringaleAPIDb db)
 {
-    var plat = await db.Plats.FindAsync(id_pl);
+    var plat = await db.Plats.FindAsync(id);
 
-    if (plat is null) return Results.NotFound();
+    if (plat is null) return TypedResults.NotFound();
 
     plat.Nom_pl = inputPlat.Nom_pl;
     plat.Prix_pl = inputPlat.Prix_pl;
     plat.Categorie_pl = inputPlat.Categorie_pl;
 
     await db.SaveChangesAsync();
+    return TypedResults.NoContent();
+}
 
-    return Results.NoContent();
-});
 
-// Supprimer un plat par ID 
-app.MapDelete("/Plats/{id}", async (int id_pl, FringaleAPIDb db) =>
+
+// Supprimer une commande par son ID
+static async Task<IResult> DeletePlat(int id, FringaleAPIDb db)
 {
-    if (await db.Plats.FindAsync(id_pl) is Plat plat)
+    if (await db.Plats.FindAsync(id) is Plat plat)
     {
         db.Plats.Remove(plat);
         await db.SaveChangesAsync();
-        return Results.NoContent();
+        return TypedResults.NoContent();
     }
 
-    return Results.NotFound();
-});
+    return TypedResults.NotFound();
+}
 
 app.Run();
